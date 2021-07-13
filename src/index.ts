93d26af57150963b886337de1d2d49f5c79571d7
benNie <bennie@openrice.com>
2021-07-13 15:58:28 +0800

    feat(examples-base): base
    添加 base example 用来测试 urlBuild function

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import { urlBuild } from './helpers/url'
 import { AxiosRequireConfig } from './types'
 import { xhr } from './xhr'
 
-function axios(config: AxiosRequireConfig) {
-  // TODO
+export default function axios(config: AxiosRequireConfig) {
+  processConfig(config)
   xhr(config)
+}
+
+function processConfig(config: AxiosRequireConfig): void {
+  config.url = transformURL(config)
+}
+
+function transformURL(config: AxiosRequireConfig): string {
+  const { url, params } = config
+  return urlBuild(url, params)
 }
